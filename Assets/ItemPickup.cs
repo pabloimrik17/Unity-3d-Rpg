b76c84f998c8acf38991b0e4b66665d899cb6836
Etherless <pabloimrik17@gmail.com>
2018-11-03 19:16:48 +0100

    Added ItemPickup to handle the interaction and the pick up of Interactable items. Added GameManager to scene Added inventory Singleton to handle add/remove items Added Item type

---
+++ Assets/ItemPickup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ItemPickup : Interactable {
+    public Item item;
+
+    public override void Interact() {
+        base.Interact();
+
+        PickUp();
+    }
+
+    private void PickUp() {
+        Debug.Log("Picking up item." + item.name);
+
+        if (Inventory.instance.add(item)) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+    #region Singleton
+    public static Inventory instance;
+
+    private void Awake() {
+        if (instance != null) {
+            Debug.Log("ERROR MORE THAN ONE INSTANCE");
+            return;
+        }
+
+        instance = this;
+    }
+
+    #endregion
+
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+
+    public int space = 20;
+    public List<Item> items = new List<Item>();
+
+    public bool add(Item item) {
+        if(!item.isDefaultItem) {        
+            if (items.Count >= space) {
+                Debug.Log("FULL INVENTORY");
+                return false;
+            }
+
+            items.Add(item);
+
+            if (onItemChangedCallback != null) {
+                onItemChangedCallback.Invoke();
+            }
+        }
+
+        return true;
+        
+    }
+
+    public void remove(Item item) {
+        items.Remove(item);
+
+        if (onItemChangedCallback != null) {
+            onItemChangedCallback.Invoke();
+        }
+    }
+}
